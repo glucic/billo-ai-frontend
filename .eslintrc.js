d3ff3189ec947762bd0a7eb390c3999de2fc6370
glucic <gabriellucic@protonmail.com>
2025-10-31 21:19:11 +0100

    eslint config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,83 @@
 module.exports = {
-  root: true,
-  parser: "@babel/eslint-parser",
-  settings: {
-    react: {
-      version: "detect",
+    root: true,
+    parser: '@babel/eslint-parser',
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+            alias: {
+                map: [['@', './src']],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
     },
-  },
-  env: {
-    node: true,
-    browser: true,
-    es6: true,
-    commonjs: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@next/next/recommended",
-    "prettier",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        node: true,
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 2020,
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ["@babel/preset-react"],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@next/next/recommended',
+        'plugin:import/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+            presets: ['@babel/preset-react'],
+        },
     },
-  },
-  rules: {
-    "import/prefer-default-export": 0,
-    "no-console": "warn",
-    "no-nested-ternary": 0,
-    "no-underscore-dangle": 0,
-    "no-unused-expressions": ["error", { allowTernary: true }],
-    camelcase: 0,
-    "react/self-closing-comp": 1,
-    "react/jsx-filename-extension": [1, { extensions: [".js", "jsx"] }],
-    "react/prop-types": 0,
-    "react/destructuring-assignment": 0,
-    "react/jsx-no-comment-textnodes": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/no-array-index-key": 0,
-    "react/no-unescaped-entities": 0,
-    "react/require-default-props": 0,
-    "react/react-in-jsx-scope": 0,
-    "linebreak-style": ["error", "unix"],
-    semi: ["error", "never"],
-  },
-};
+    plugins: ['react', 'react-hooks', 'import', 'jsx-a11y'],
+    rules: {
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+        ],
+        'no-nested-ternary': 'off',
+        'no-underscore-dangle': 'off',
+        'no-unused-expressions': ['error', { allowTernary: true }],
+        camelcase: 'off',
+
+        'react/self-closing-comp': 'warn',
+        'react/jsx-filename-extension': [
+            'warn',
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
+        'react/prop-types': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/no-array-index-key': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'import/prefer-default-export': 'off',
+        'import/order': [
+            'warn',
+            {
+                groups: [
+                    ['builtin', 'external'],
+                    ['internal'],
+                    ['parent', 'sibling', 'index'],
+                ],
+                'newlines-between': 'always',
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+        ],
+        'linebreak-style': ['error', 'unix'],
+        semi: ['error', 'never'],
+    },
+}
